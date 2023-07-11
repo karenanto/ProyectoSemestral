d45944831796a9737f6db616146d18c5c39df573
aalcainos <anto.alcainos@gmail.com>
2023-07-11 19:12:02 -0400

    CRUD usuarios
    ayuda

--- ProyectoSemestralDjango/api/serializers.py
+++ ProyectoSemestralDjango/api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from apps.Tienda.models import Producto
+from apps.Tienda.models import Usuario
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = '__all__'
 
-
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
 
--- ProyectoSemestralDjango/api/views.py
+++ ProyectoSemestralDjango/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 #from rest_framework import generics
 from apps.Tienda.models import Producto
 from .serializers import ProductSerializer
+from apps.Tienda.models import Usuario
+from .serializers import UsuarioSerializer
 from django.http import JsonResponse
 # Create your views here.
 
@@ -38,3 +40,32 @@ def eliminar_producto(request, pk):
         producto.delete()
         return JsonResponse({'Producto eliminado exitosamente'}, status=204)
     return JsonResponse({'Solicitud inválida'}, status=400)
+
+def crearUsuario(request):
+    if request.method == 'POST':
+        serializer =  UsuarioSerializer(data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+def actualizarUsuario(request, pk):
+    usuario= get_object_or_404(Usuario, pk=pk)
+    if request.method == 'PUT':
+        serializer = UsuarioSerializer(usuario, data=request.PUT)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer._errors, status=400)
+
+def eliminarUsuario(request, pk):
+    producto = get_object_or_404(Usuario, pk=pk)
+    if request.method == 'DELETE':
+        usuario.delete()
+        return JsonResponse({'Usuario eliminado exitosamente'}, status=204)
+    return JsonResponse({'Solicitud inválida'}, status=400)
+
+def listarUsuario(request):
+    usuario = Usuario.objects.all()
+    serializer = UsuarioSerializer(Usuario, many=True)
+    return JsonResponse(serializer.data, safe=False)
